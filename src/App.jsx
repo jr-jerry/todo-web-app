bb94bea7be903aeb8fdde336372bd123d1f55f99
jr-jerry <jerryverma798@gmail.com>
2025-07-15 14:17:00 +0530

    fixing import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Homepage from './components/homepage'
+import Homepage from './components/homepage.jsx'
 import { ThemeProvider } from './context/ThemeContext'
 import { TodoProvider } from './context/TodoContext'
 import { DetailProvider } from './context/DetailContext'
